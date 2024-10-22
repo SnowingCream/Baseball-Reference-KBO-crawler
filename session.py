d9bbc582fc7bb933551a061c1a76d4ed39614416
Snowing Cream <68310688+Scombo2020@users.noreply.github.com>
2024-10-21 22:29:50 -0500

    initial commit

---
+++ session.py
@@ -0,0 +1,43 @@
+import datetime, time, requests
+
+# only 10 requests per minute.
+# started from: https://github.com/jldbc/pybaseball/blob/master/pybaseball/datasources/bref.py
+class Session():
+
+    def __init__(self) -> None:
+        self.cnt = 0
+        self.session = requests.Session()
+
+    '''
+    Helper function 1
+    Behavior: handle maximum api call rate: 10 calls per 1 min.
+    Usage: called inside fetch().
+    '''
+    def handle_call_rate(self) -> None:
+        
+        self.cnt += 1
+        
+        if self.cnt % 10 == 0:
+            print(F"have called {self.cnt} apis: 1 min break starts") 
+            time.sleep(60) 
+        
+    '''
+    Helper function 2
+    Behavior: general api call with given url -> return data in json format(dict)
+    Usage: called inside other api call functions.
+    '''
+    def fetch(self, url) -> requests.Response:
+
+        response = self.session.get(url)
+        self.handle_call_rate()
+    
+        if response.status_code != 200:
+            print(F"response status not 200: {response.status_code}")
+            
+            # occasionally api server throws this error code
+            if response.status_code == 500:
+                print("instant server error, trying again.")
+                time.sleep(3)
+                fetch(url)
+            
+        return response
