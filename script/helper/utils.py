005c437df0258d4cfb6266be17a641a14c8ffd3e
Snowing Cream <68310688+Scombo2020@users.noreply.github.com>
2024-11-11 20:03:38 -0600

    implemented 'u'-handling function.

--- script/helper/utils.py
+++ script/helper/utils.py
@@ -22,6 +22,7 @@ last_name_dict_kor = {
     "pak": "park", # pak is never 백? not sure -> manually check, no conflict (good)
     "im": "lim",
     "sim": "shim",
+    "sin": "shin",
     "gwon": "kwon",
     "jo": "cho",
     "gu": "koo",
@@ -40,19 +41,59 @@ last_name_dict_kor = {
     "seonu": "seonwoo",
     "jeun": "jeon",
     "hur": "heo",
+    "her": "heo",
+    "gi": "ki",
+    "tong": "dong",
+    "chen": "cheon",
+    "chun": "cheon",
+    "chi": "ji",
+    "geum": "keum",
+    "ju": "joo",
     
 }
 
-given_name_dict_kor = {
-    "u": "woo",
+first_name_dict_kor = {
+    "u": "woo", # no 어? guess it's fine
     "been": "bin",
     "see": "si",
+    "sin": "shin",
     "wahn": "wan",
+    "yu": "yoo",
+    "young": "yeong",
+    "gi": "ki",
+    "suh": "seo",
+    "seop": "seob",
+    "sup": "seob",
+    "gyu": "kyu",
+    "geun": "keun",
+    "ttaum": "tteum",
+    # "gap": "gab", # there is 갑 (홍성갑)
+    "june": "joon", 
+    "pill": "pil",
+    "bu": "boo", # no 버, so it's fine
+    "sub": "seob", # no 숩, so it's fine
+    "guk": "kook", # no 걱, so it's fine
+    "hyuk": "hyeok", # no 휵, so it's fine
+    "ju": "joo", # no 저, so it's fine
+    
 }
 
-non_korean_name_dict_eng = {
-    "aarona ltherr" = "aaron altherr",
-    "andrew" = "andy
+# 1-to-1 correction for very unique cases: usually typo, with some rare non-Korean players' names
+manual_handle_dict = {
+    "aarona ltherr": "aaron altherr", # space typo
+    "seok sang ho": "sang ho seok", # flipped format
+    "seol jae min": "jae min seol", # flipped format
+    "no gun woo": "gun woo no", # flipped format
+    "noh jae won": "jae won noh", # flipped format
+    "nam yun sung": "yun sung nam", # flipped format
+    "park tae won": "tae won park", # flipped format
+    "park seunghoon": "seung hoon park", # fillped format, no -
+    "pilljoon jang": "pill joon jang", # no -
+    "shin  ho lim": "shin ho lim", # extra space
+    "shin wojae": "woo jae shin", # flipped format, no -
+    
+    
+    
 }
 
 '''
@@ -171,7 +212,7 @@ def eng_get_first_and_last(name: str) -> str:
     split = name.split(" ")
 
     # 'jr' is not helpful for distinguishing players
-    if split[-1].contains["jr"]:
+    if 'jr' in split[-1]:
         split = split[-1]
     
     if len(split) > 2:
@@ -183,7 +224,7 @@ def eng_get_first_and_last(name: str) -> str:
 
 '''
 Helper function 4
-Behavior: Parse the given name and return the last name (in terms of character, not word) -> use it for Korean name only.
+Behavior: Parse the given full name and return the last name (in terms of character, not word) -> use it for Korean name only.
 Usage: used to check if players with rare last name exist (otherwise merge them into the common notation for that last name to resolve naming conflict)
 '''
 def kor_get_last(name_kor_list: list[str], last_name: str) -> list[str]:
@@ -196,50 +237,98 @@ def kor_get_last(name_kor_list: list[str], last_name: str) -> list[str]:
 
 '''
 Helper function 5
-Behavior: Parse the given name and check if the last name is in the last_name_dict for generalization (a consistent notation for last names), and if so, update it.
+Behavior: Parse the given full name and check if the last name is in the last_name_dict_kor for generalization (a consistent notation for last names), and if so, update it.
 Usage: used to generalize last names to reduce naming conflict.
 '''
 def kor_update_last(name: str) -> str:
     
     name_split = name.split(" ")
 
-    if len(name_split) > 1 and name_split[-1] in last_name_dict:
-        name_split[-1] = last_name_dict[name_split[-1]]
+    if len(name_split) > 1 and name_split[-1] in last_name_dict_kor:
+        name_split[-1] = last_name_dict_kor[name_split[-1]]
         return " ".join(name_split)
     else:
         return name
 
-
-def kor_update_given(df, name: str) -> str:
-
-    df_br = df[df['name_eng_y'].isna()]
-    df_st = df[df['name_eng_x'].isna()]
-
-    df_br_set = set(df_br["name_new"].to_list())
-    df_st_set = set(df_st["name_new"].to_list())
-
-
-def kor_update_given_2(name_input: str, opposite_set: set[str]) -> str:
-
-    name_split = name_input.split(" ")
-
+'''
+Helper function 6
+Behavior: Parse the given full name and check if the first name is in the first_name_dict for generalization (a consistent notation for first names), and if so, update it.
+Usage: used to generalize first names to reduce naming conflict.
+'''
+def kor_update_first(name: str) -> str:
+    
+    name_split = name.split(" ")
     if len(name_split) > 1:
-        given_name = name_split[:-1]
-
-        for i in range(len(given_name)):
-            if given_name[i] in given_name_dict_kor:
-                given_name[i] = given_name_dict_kor[given_name[i]]
-
-            if given_name[i].contains('u'):
-                chr_cdd_1 = given_name[i][:].replace('u', 'eo')
-                chr_cdd_2 = given_name[i][:].replace('u', 'oo')
-
-                name_cdd = given_name[:]
+        first_name_split = name_split[:-1]
+        for i in range(len(first_name_split)):
+            if first_name_split[i] in first_name_dict_kor:
+                first_name_split[i] = first_name_dict_kor[first_name_split[i]]
+        return " ".join(first_name_split) + " " + name_split[-1]
+    return name
                 
     
-    else:
-        return name_input
+'''
+Helper function 7
+Usage: specifically handling 'u'
+'''
+def kor_handle_u(row, candidates: set[str]) -> str:
 
+    name_split = row['name_new'].split(" ")
+    
+    # remove the last name
+    if len(name_split) > 1:
+        first_name_split = name_split[:-1]
+
+        # 'eu' is fine, it has only 1 matching pronuncation ('ㅡ')
+        # if the first name has more than 1 Korean character, need to check if there are several 'u's.
+        # if there is no 'u' or more than 1 'u', cannot handle it -> return the original value.
+        if len(first_name_split) > 1:
+            count = 0
+            for name in first_name_split:
+                if 'u' in name and 'eu' not in name:
+                    count += 1
+            if not count:
+                # print(f"given name: {row['name_new']} -> at least than 2 charcters, no 'u'")
+                return row['name_new']
+            if count == len(first_name_split):
+                print(f"given name: {row['name_new']} -> at least than 2 charcters, all 'u' (count: {count})")
+                return row['name_new']
+        else:
+            if 'u' not in first_name_split[0]:
+                # print(f"given name: {row['name_new']} -> 1 character with no 'u'")
+                return row['name_new']
+                
+        # once confirmed there is only 1 'u', find that character.
+        # then substitute that u with 'oo' and 'eo'.
+        # if there are both or neither, cannot handle it -> return the original value.
+        # if there is only 1 of them, return that name.
+        for i in range(len(first_name_split)):
+            if 'u' in first_name_split[i] and 'eu' not in first_name_split[i]:
+
+                first_name_copy_oo = first_name_split[:]
+                first_name_copy_oo[i] = first_name_copy_oo[i].replace('u', 'oo')
+                first_name_copy_oo = " ".join(first_name_copy_oo) + " " + name_split[-1]
+
+                first_name_copy_eo = first_name_split[:]
+                first_name_copy_eo[i] = first_name_copy_eo[i].replace('u', 'eo')
+                first_name_copy_eo = " ".join(first_name_copy_eo) + " " + name_split[-1]
+
+                # print(f"given name: {row['name_new']} -> oo: {first_name_copy_oo}, eo: {first_name_copy_eo}")
+
+                if first_name_copy_oo not in candidates and first_name_copy_eo not in candidates:
+                    print(f"given name: {row['name_new']} -> neither exists")
+                    return row['name_new']
+                if first_name_copy_oo in candidates and first_name_copy_eo in candidates:
+                    print(f"given name: {row['name_new']} -> both exist")
+                    return row['name_new']
+                if first_name_copy_oo in candidates:
+                    return first_name_copy_oo
+                if first_name_copy_eo in candidates:
+                    return first_name_copy_eo
+
+        # print(f"given name: {row['name_new']} -> what case is this???") -> never happens
+
+    return row['name_new']
     
     
     
