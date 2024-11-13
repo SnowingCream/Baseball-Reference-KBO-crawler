5b7fbda85b42b91b9c7027c1beda6ffc64cafb49
Snowing Cream <68310688+Scombo2020@users.noreply.github.com>
2024-11-12 19:30:08 -0600

    added more unique case handling functionalities

--- script/helper/utils.py
+++ script/helper/utils.py
@@ -42,13 +42,56 @@ last_name_dict_kor = {
     "jeun": "jeon",
     "hur": "heo",
     "her": "heo",
+    "hor": "heo",
+    "huh": "heo",
+    "ho": "heo", # check 호 -> no such last name (good)
     "gi": "ki",
     "tong": "dong",
     "chen": "cheon",
     "chun": "cheon",
     "chi": "ji",
+    "wee": "wi",
     "geum": "keum",
     "ju": "joo",
+    "youm": "yeom",
+    "kum": "keum",
+    "son": "sohn",
+    "shon": "sohn", # only 1 case
+    "kweon": "kwon", # only 1 case
+    "mon": "moon", # only 1 case
+    "baik": "baek", # only 1 case
+    "koh": "ko", # only 1 case
+    "dang": "tang", # only 1 case
+    "tian": "cheon", # only 1 case
+    "ro": "noh", # only 1 case
+    "sok": "seok", # only 1 case
+    "o": "oh", # only 1 case
+    "weon": "won", # only 1 case
+    "ga": "ka", # only 1 case
+    "uhm": "eom", # only 1 case
+    "um": "eom", # only 1 case
+    "joung": "jeong", # only 1 case
+    "pyon": "byeon", # only 1 case
+    "yi": "lee", # only 1 case
+    "jong": "jeong", # only 1 case
+    "liu": "yoo", # only 1 case
+    "kuk": "kook", # only 1 case
+    "beak": "baek", # only 1 case
+    "yuk": "yook", # only 1 case
+    "chei": "chae", # only 1 case
+    "gam": "kam", # only 1 case
+    "yo": "yeo", # only 2 cases
+    "to": "doh", # only 2 cases
+    "do": "doh", # only 1 case
+    "in": "yin", # only 2 cases
+    "ryou": "ryu", # only 2 cases
+    "gil": "kil",
+    "sun": "seon",
+    "you": "yoo",
+    "aum": "eom",
+    
+    
+
     
 }
 
@@ -75,6 +118,9 @@ first_name_dict_kor = {
     "guk": "kook", # no 걱, so it's fine
     "hyuk": "hyeok", # no 휵, so it's fine
     "ju": "joo", # no 저, so it's fine
+    "hsiang": "sang",
+    "oug": "wook",
+    "yol": "yeol",
     
 }
 
@@ -90,10 +136,113 @@ manual_handle_dict = {
     "park seunghoon": "seung hoon park", # fillped format, no -
     "pilljoon jang": "pill joon jang", # no -
     "shin  ho lim": "shin ho lim", # extra space
+    "dan woo  kim": "dan woo kim", # extra space
     "shin wojae": "woo jae shin", # flipped format, no -
+    "chae wonhu": "won hoo chae", # flipped format, no -
+    "das sung koo": "dae seong koo", # typo
+    "jose para": "jose parra", # typo
+    "julio depaula": "julio de paula", # missing space
+    "rick vandenhurk": "rick van den hurk", # missing space
+    "kevin dattolla": "kevin dattola", # typo
+    "ki hyo ahn": "ki hyo nam", # typo
+    "kiyoung moon": "ki young moon", # missing space
+    "leang keu beul": "matt blank", # ??????? 
+    "lang gyun gil": "rang gyun kil", # unique name, manual match
+    "r": "reu", # hard to handle
+    "michael bowdent": "michael bowden", # typo
+    "sangr yul jeon": "sang rul jeon", # space typo
+    "scoot baker": "scott baker", # typo
+    "satohshi iriki": "satoshi iriki", # unique name, manual match
+    "tylor wilson": "tyler wilson", # typo
+    "tyrone hornes": "tyrone horne", # typo
+    "yu naml": "yool nam", # space typo
+    "yeop sagong": "gong yeop sa", # space typo
+    "young jae gp": "young jae ko", # typo
+    "soo hyung kjim": "soo hyung kim", # typo
+    "park sion": "si on park", # flipped format, no -
+    "lee ju heon": "joo heon lee", # flipped format
+    "lee hyeonmin": "hyeon min lee", # flipped format, no -
+    "kim gyudae": "gyoo dae kim", # flipped format, no -
+    "kim donghyeok": "dong hyeok kim", # flipped format, no -
+    "jun rugun": "roo geon jeon", # flipped format, no -
+    "jun pyo jhun": "jun pyo jeon", # typo
+    "jo sung hyeon": "seong hyeon jo", # flipped format
+    "jo hyo won": "hyo won jo", # flipped format
+    "hwang sung woong": "seong woong hwang", # flipped format
+    "je hwan yoo": "hwan yoo je", # flipped format
+    "hong ji hun": "ji hoon hong", # flipped format
+    "hong jae young": "jae young hong", # flipped format
+    "han minwoo": "min woo han", # flipped format
+    "eom tae kyoung": "tae kyeong eom", # flipped format
+    "eom tae ho": "tae ho eom", # flipped format
+    "yeong jin do": "yeong jin doo", # unique name, manual match
+    "jung bong": "joong keun bong", # typo
+    "jin jong kil": "jong kil jin", # flipped format
+    "deuk han yong": "deok han yong", # unique name, manual match
+    "yeo seung cheol": "seung cheol yeo", # flipped format
+    "choi yun cheol": "yoon cheol choi", # flipped format
+    "gil ho song": "gil ho son", # typo
+    "jeung woo seok": "jeong woo seok", # unique name, manual match
+    "lee jung seok": "joong seok lee", # flipped format
+    "sang guk pang": "sang kook park", # typo
     
     
     
+    
+    
+    
+    
+    
+    
+    
+}
+
+first_name_dict_eng = {
+    "albert": "al",
+    "alessandro": "alex",
+    "andrew": "andy",
+    "andrew": "drew",
+    "bennett": "ben",
+    "benjamin": "ben",
+    "bradley": "brad",
+    "chadwick": "chad",
+    "christopher": "chris",
+    # "daniel": "dan",
+    "roberto": "robert",
+    "raymond": "ray",
+    # "daniel": "danny",
+    "david": "dave",
+    "douglas": "doug",
+    "eduardo": "eddie",
+    "bienvenido": "ben",
+    "clifford": "cliff",
+    "jacob": "jake",
+    "jeffrey": "jeff",
+    "james": "jimmy",
+    "jonathan": "jon",
+    "joshua": "josh",
+    # "kenneth": "kenny",
+    # "kenneth": "ken",
+    "kirkland": "kirk",
+    "leslie": "les",
+    "lenin": "len",
+    "louis": "lou",
+    "matthew": "matt",
+    "martin": "marty",
+    "melquicedec": "melqui",
+    "michael": "mike",
+    "philip:": "pil",
+    "patrick": "pat",
+    "richard": "rick",
+    "ronald": "ron",
+    "samual": "sam",
+    "timothy": "tim",
+    # "thomas": "tom",
+    # "thomas": "tommy",
+    "tyrone": "ty",
+    "vincent": "vinny",
+    "william": "will",
+    "zachary": "zach",
 }
 
 '''
@@ -331,7 +480,12 @@ def kor_handle_u(row, candidates: set[str]) -> str:
     return row['name_new']
     
     
-    
+def manual_check(name: str) -> str:
+
+    if name in manual_handle_dict:
+        return manual_handle_dict[name]
+
+    return name
 
 
 
